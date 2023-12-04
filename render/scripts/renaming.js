9948bb1de505d38ce2da063870a0e5ff6717ce56
Lucas Griffin <66416659+LucasGriffinDev@users.noreply.github.com>
2023-12-05 08:52:55 +1100

    working renaming feature!

--- render/scripts/renaming.js
+++ render/scripts/renaming.js
@@ -1,5 +1,3 @@
-
-
 const os = require('os');
 
 
@@ -9,11 +7,52 @@ const downloadsPath = path.join(os.homedir(), 'Downloads');
 const targetFolder = document.getElementById('targetFolder');
 targetFolder.textContent = downloadsPath;
 
+
+const renameMappings = JSON.parse(fs.readFileSync(mappingsPath, 'utf8'));
+
 // Regex to match folders with six-digit names
 const folderRegex = /\d{6}/;
 
 // Function to print contents of all folders that match the regex
+
+function renameFilesInFolder(folderPath) {
+    fs.readdir(folderPath, (err, files) => {
+        if (err) {
+            console.error('Error reading the folder:', err);
+            return;
+        }
+
+        files.forEach(file => {
+            // Ignore directories
+            if (fs.statSync(path.join(folderPath, file)).isDirectory()) {
+                return;
+            }
+
+            renameMappings.forEach(mapping => {
+                mapping.variations.forEach(variation => {
+                    if (file.includes(variation)) {
+                        const newFileName = mapping.targetName + path.extname(file);
+                        const newFilePath = path.join(folderPath, newFileName);
+
+                        fs.rename(path.join(folderPath, file), newFilePath, err => {
+                            if (err) {
+                                console.error(`Error renaming ${file} to ${newFileName}:`, err);
+                            } else {
+                                console.log(`Renamed ${file} to ${newFileName}`);
+                            }
+                        });
+                    }
+                });
+            });
+        });
+    });
+}
+
 function printFolderContents() {
+
+
+
+
     if (fs.existsSync(mappingsPath)) {
         console.log('mappingsPath:', readMappings());
     } else {
@@ -29,25 +68,16 @@ function printFolderContents() {
         // Filter for directories that match the regex
         const folders = entries.filter(entry => entry.isDirectory() && folderRegex.test(entry.name));
 
-        // Loop through each matching folder and print its contents
+        // Loop through each matching folder and rename its contents
         folders.forEach(folder => {
             const folderPath = path.join(downloadsPath, folder.name);
-            console.log(`Contents of folder: ${folder.name}`);
+            console.log(`Processing folder: ${folder.name}`);
 
-            // Read the contents of the folder
-            fs.readdir(folderPath, (err, files) => {
-                if (err) {
-                    console.error(`Error reading the folder ${folder.name}:`, err);
-                } else {
-                    // Print out each file in the folder
-                    files.forEach(file => {
-                        console.log(`- ${file}`);
-                    });
-                }
-            });
+            // Call renameFilesInFolder for each folder
+            renameFilesInFolder(folderPath);
         });
     });
 }
 
 // Call the function to print folder contents
-printFolderContents();
+// printFolderContents();
