e2f1ae0d60ae45ba44ea68f3f09d7370060ce781
Lucas Griffin <66416659+LucasGriffinDev@users.noreply.github.com>
2023-12-18 05:03:57 +1100

    organise

--- scripts/render/toggle.js
+++ scripts/render/toggle.js
@@ -3,17 +3,7 @@ const { processFoldersForRenaming } = require('../scripts/naming/renaming.js');
 let appStatus = false
 
 document.getElementById('toggleButton').addEventListener('click', () => {
-    const slider = document.getElementById('slider');
+    const slider = document.getElementById('start-app');
     const targetFolderPath = document.getElementById('targetFolder').textContent; // Get the current target folder path
-
-    if (appStatus === false) {
-        appStatus = !appStatus;
         processFoldersForRenaming(targetFolderPath); // Pass the target folder path to the function
-        slider.style.backgroundColor = 'green';
-        slider.innerText = 'ON';
-    } else {
-        appStatus = !appStatus;
-        slider.style.backgroundColor = 'red';
-        slider.innerText = 'OFF';
-    }
 });
